3ec351aec01a7a6352c3450367b3d270e01580e7
Bin <iamsk.info@gmail.com>
2018-11-28 12:19:39 +0800

    update models

--- apps/configuration/models.py
+++ apps/configuration/models.py
@@ -8,13 +8,19 @@ from jsonfield import JSONField
 class Schema(models.Model):
     data = JSONField()
 
+    def _get_name(self, data):
+        print data
+        name = ''
+        if data['type'] == 'object':
+            name += '-'.join(data['properties'].keys())
+        elif data['type'] == 'array':
+            name += '[{}]'.format(data['items']['type'])
+            name += self._get_name(data['items'])
+        return '-' + name if name else ''
+
     def __unicode__(self):
-        base_name = '[{}]-[{}]-'.format(self.data['title'], self.data['type'])
-        if self.data['type'] == 'object':
-            base_name += '-'.join(self.data['properties'].keys())
-        elif self.data['type'] == 'array':
-            base_name += '-'.join(self.data['items']['properties'].keys())
-        return base_name
+        base_name = '[{}]'.format(self.data['type'])
+        return base_name + self._get_name(self.data)
 
 
 class Configuration(models.Model):
